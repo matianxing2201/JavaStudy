f69395e3a6a0b1664f05e9d9325c4325cbed93b5
Matianxing <450086055@qq.com>
2024-10-23 22:17:12 +0800

    feat: 快速入门02

---
+++ spring/src/com/mtx/spring/test/SpringBeanTest.java
@@ -0,0 +1,32 @@
+package com.mtx.spring.test;
+
+import com.mtx.spring.bean.Monster;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringBeanTest {
+    public static void main(String[] args) {
+        getMonster();
+    }
+
+    public static void getMonster() {
+        // 1. 创建容器 ApplicationContext
+        // 2. 该容器和容器配置文件关联
+
+        ApplicationContext ioc = new ClassPathXmlApplicationContext("beans.xml");
+
+        // 3. 通过getBean()方法获取对应的对象
+        //    默认返回Object类型， 运行类型Monster
+//        Object monster01 = ioc.getBean("monster01");
+        Monster monster01 = (Monster)ioc.getBean("monster01");
+
+        // 4. 输出
+        System.out.println("monster01=" + monster01 + "运行类型：" + monster01.getClass().getName());
+        System.out.println("monster01=" + monster01 + "name:" + monster01.getName() + " id:" + monster01.getId());
+
+        // 5. 也可以在获取的时候 直接指定class类型  可以多次获取同一个bean
+        Monster monster02 = ioc.getBean("monster01", Monster.class);
+        System.out.println("name:" + monster02.getName() + " id:" + monster02.getId());
+
+    }
+}
