0791115769c88352c4e11c8353b88332df5dd079
Matianxing <450086055@qq.com>
2024-10-24 21:33:13 +0800

    feat: 获取bean信息api

--- spring/src/com/mtx/spring/test/SpringBeanTest.java
+++ spring/src/com/mtx/spring/test/SpringBeanTest.java
@@ -1,15 +1,17 @@
 package com.mtx.spring.test;
 
 import com.mtx.spring.bean.Monster;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringBeanTest {
     public static void main(String[] args) {
-        getMonster();
+
     }
 
-    public static void getMonster() {
+    @Test
+    public void getMonster() {
         // 1. 创建容器 ApplicationContext
         // 2. 该容器和容器配置文件关联
 
@@ -27,6 +29,12 @@ public class SpringBeanTest {
         // 5. 也可以在获取的时候 直接指定class类型  可以多次获取同一个bean
         Monster monster02 = ioc.getBean("monster01", Monster.class);
         System.out.println("name:" + monster02.getName() + " id:" + monster02.getId());
+
+        // 6. 查看容器侏儒了那些bean对象，输出bean的名称
+        String[] beanDefinitonsNames = ioc.getBeanDefinitionNames();
+        for (String beanName : beanDefinitonsNames) {
+            System.out.println("beanDefinitonsName=" + beanName);
+        }
 
     }
 }
